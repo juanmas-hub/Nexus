9c84a415f56107393254aedf282423567c98cd69
Juanmas-hub <juancitobravoc@gmail.com>
2025-12-23 03:29:45 +0000

    feat: add user automigrate

--- backend/auth-service/cmd/main.go
+++ backend/auth-service/cmd/main.go
@@ -11,19 +11,28 @@ import (
 
 	"nexus/auth-service/internal/adapters/handler/http"
 	"nexus/auth-service/internal/adapters/repository/postgres"
+	"nexus/auth-service/internal/core/domain"
 	"nexus/auth-service/internal/core/services"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error cargando .env")
+		log.Println("Aviso: No se encontró archivo .env")
 	}
 
-	dsn := os.Getenv("DATABASE_URL") 
+	dsn := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("No se pudo conectar a la DB:", err)
 	}
+
+	log.Println("Ejecutando migraciones...")
+	err = db.AutoMigrate(&domain.User{})
+	if err != nil {
+		log.Fatal("Error en la migración de la base de datos:", err)
+	}
+	log.Println("Migración completada con éxito.")
+
 
 	userRepo := repository.NewPostgresRepository(db)
 	authService := services.NewAuthService(userRepo)
@@ -31,8 +40,8 @@ func main() {
 
 	r := gin.Default()
 
-	
 	r.POST("/auth/login", authHandler.Login)
 
+	log.Println("Servidor iniciado en el puerto :8080")
 	r.Run(":8080")
 }
