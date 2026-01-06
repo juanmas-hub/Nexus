c7e037a1316b1e6246a1c2db94fd5c146902bd2b
Juanmas-hub <juancitobravoc@gmail.com>
2026-01-06 23:26:22 +0000

    chore: remove target folder and add to gitignore

--- backend/catalog-service/target/debug/deps/catalog_service-a3d08b0ca1da832c.d
+++
@@ -1,5 +0,0 @@
-/home/juanma/Nexus/backend/catalog-service/target/debug/deps/catalog_service-a3d08b0ca1da832c.d: src/main.rs
-
-/home/juanma/Nexus/backend/catalog-service/target/debug/deps/libcatalog_service-a3d08b0ca1da832c.rmeta: src/main.rs
-
-src/main.rs:
