b21fd6f0106c949aad938538a13841bdae48e63a
Juanmas-hub <juancitobravoc@gmail.com>
2026-01-06 23:15:36 +0000

    chore: cargo init

---
+++ backend/catalog-service/target/debug/deps/catalog_service-30f4aa2bfebfc836.d
@@ -0,0 +1,5 @@
+/home/juanma/Nexus/backend/catalog-service/target/debug/deps/catalog_service-30f4aa2bfebfc836.d: src/main.rs
+
+/home/juanma/Nexus/backend/catalog-service/target/debug/deps/libcatalog_service-30f4aa2bfebfc836.rmeta: src/main.rs
+
+src/main.rs:
