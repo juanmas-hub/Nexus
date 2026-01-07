23eeb6d24cc6c26c3eee01c9ff374d60419d17ec
Juanmas-hub <juancitobravoc@gmail.com>
2026-01-07 19:29:13 +0000

    feat: implemented uuid

--- backend/catalog-service/src/domain/event.rs
+++ backend/catalog-service/src/domain/event.rs
@@ -1,12 +1,13 @@
 use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Utc};
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Event {
-    pub id: String,
-    pub title: String,
-    pub description: String,
-    pub location: String,
-    pub date: DateTime<Utc>,
+    pub id: Uuid,  
+    pub title: String,      
+    pub description: String, 
+    pub location: String, 
+    pub event_date: DateTime<Utc>,
     pub capacity: i32,
 }
