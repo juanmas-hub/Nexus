382d28dea15f4428359b0c55ce9378479280c615
Juanmas-hub <juancitobravoc@gmail.com>
2026-01-11 17:46:29 +0000

    feat: add register structs

--- backend/api-gateway/internal/core/domain/auth.go
+++ backend/api-gateway/internal/core/domain/auth.go
@@ -5,6 +5,13 @@ type LoginRequest struct {
     Password string `json:"password"`
 }
 
+type registerRequest struct {
+    Email     string `json:"email" binding:"required,email"`
+    Password  string `json:"password" binding:"required,min=6"`
+    FirstName string `json:"first_name" binding:"required"`
+    LastName  string `json:"last_name" binding:"required"`
+}
+
 type UserDTO struct {
     ID    string   `json:"id"`
     Email string `json:"email"`
@@ -13,4 +20,14 @@ type UserDTO struct {
 type LoginResponse struct {
     Token string  `json:"token"`
     User  UserDTO `json:"user"`
+}
+
+type RegisterResponse struct {
+    Message string `json:"message"`
+    User    struct {
+        ID        string `json:"id"`
+        Email     string `json:"email"`
+        FirstName string `json:"first_name"`
+        LastName  string `json:"last_name"`
+    } `json:"user"`
 }
